37d2a0e8db50ee78eff3240ba67cdbbd84d1959e
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-03-04 03:54:39 +0200

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  entry: ['bootstrap-loader', './app.js'],
+  output: {
+    path: './build',
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  performance: {
+    hints: 'warning',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitErrors: true,
+          failOnError: true,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+};
