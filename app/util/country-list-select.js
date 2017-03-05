f8e49291b8ab3912bed21cf39d948942ddab3643
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-03-05 23:20:02 +0200

    rename countries util function

--- app/util/country-list-select.js
+++ app/util/country-list-select.js
@@ -4,7 +4,7 @@ import countries from 'country-list';
 /**
  * Stateless utility function to get country list for select tag.
  */
-export default function getCountriesSelect() {
+export default function countryListSelect() {
   const countriesList = countries();
   return countriesList.getNames().map((countryName) => (
     <option value={countryName} key={countryName}>{countryName}</option>
